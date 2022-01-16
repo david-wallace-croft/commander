98c0360ab0948aafff270894c4485b72cb64397e
David Wallace Croft <david@CroftSoft.com>
2022-01-15 19:17:30 -0600

    clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+  #[clap(short, long, default_value = "World")]
+  name: String,
+  #[clap(short, long, default_value_t = 1)]
+  count: u8,
+}
+
 fn main() {
-    println!("Commander © 2022 CroftSoft Inc");
+  println!("Commander © 2022 CroftSoft Inc");
+  let args = Args::parse();
+  for _ in 0..args.count {
+    println!("Hello, {}!", args.name);
+  }
 }

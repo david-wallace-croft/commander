1a1201d7b7cd5d1d68d82e4eb8fc18b7938eaf01
David Wallace Croft <david@CroftSoft.com>
2022-01-17 10:07:36 -0600

    sorted chain functions

--- src/main.rs
+++ src/main.rs
@@ -35,14 +35,14 @@ fn ask(prompt: &str, default: &str) -> String {
 
 fn main() {
   let name_arg = Arg::new(ARG_NAME_NAME)
-    .long(ARG_NAME_NAME)
     .help(ARG_NAME_HELP)
+    .long(ARG_NAME_NAME)
     .short(ARG_NAME_SHORT)
     .takes_value(ARG_NAME_TAKES_VALUE);
   let app: App = App::new(APP_NAME)
-    .author(APP_AUTHOR)
     .about(APP_ABOUT)
-    .arg(name_arg);
+    .arg(name_arg)
+    .author(APP_AUTHOR);
   let arg_matches: ArgMatches = app.get_matches();
   let name_option: Option<&str> = arg_matches.value_of(ARG_NAME_NAME);
   let name: String = match name_option {

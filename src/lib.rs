9512a4df399cdbbdc02313cdaa7090af0ffeebdd
David Wallace Croft <david@CroftSoft.com>
2022-01-18 21:27:14 -0600

    smaller main

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,14 @@
 use std::io::{stdin, stdout, Error, Stdin, Write};
 
-const NAME_DEFAULT: &str = "World";
-const NAME_PROMPT: &str = "What is your name? [World]: ";
+pub const APP_NAME: &str = "CroftSoft Commander © 2022 CroftSoft Inc";
+pub const APP_ABOUT: &str = "Command-line Rust example";
+pub const APP_AUTHOR: &str = "David Wallace Croft, david@croftsoft.com";
+pub const ARG_NAME_HELP: &str = "name to greet";
+pub const ARG_NAME_NAME: &str = "name";
+pub const ARG_NAME_SHORT: char = 'n';
+pub const ARG_NAME_TAKES_VALUE: bool = true;
+pub const NAME_DEFAULT: &str = "World";
+pub const NAME_PROMPT: &str = "What is your name? [World]: ";
 
 pub fn ask(prompt: &str, default: &str) -> String {
   loop {
@@ -24,8 +31,12 @@ pub fn ask(prompt: &str, default: &str) -> String {
   }
 }
 
-pub fn greet(name_option: Option<&str>) {
-  let name: String = match name_option {
+pub struct MainArgs<'a> {
+  pub name_option: Option<&'a str>,
+}
+
+pub fn main(main_args: MainArgs) {
+  let name: String = match main_args.name_option {
     Some(arg_name) => arg_name.to_string(),
     None => ask(NAME_PROMPT, NAME_DEFAULT),
   };
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,6 @@
 // https://docs.rs/clap/latest/clap/
 use clap::{App, Arg, ArgMatches};
-
-const APP_NAME: &str = "CroftSoft Commander © 2022 CroftSoft Inc";
-const APP_ABOUT: &str = "Command-line Rust example";
-const APP_AUTHOR: &str = "David Wallace Croft, david@croftsoft.com";
-const ARG_NAME_HELP: &str = "name to greet";
-const ARG_NAME_NAME: &str = "name";
-const ARG_NAME_SHORT: char = 'n';
-const ARG_NAME_TAKES_VALUE: bool = true;
+use commander::*;
 
 fn main() {
   let name_arg = Arg::new(ARG_NAME_NAME)
@@ -21,5 +14,5 @@ fn main() {
     .author(APP_AUTHOR);
   let arg_matches: ArgMatches = app.get_matches();
   let name_option: Option<&str> = arg_matches.value_of(ARG_NAME_NAME);
-  commander::greet(name_option);
+  commander::main(MainArgs { name_option });
 }

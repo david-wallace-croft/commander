b1bc51a977cb6cfcaa2aaf5a614121ad7996437c
David Wallace Croft <david@CroftSoft.com>
2022-01-19 19:56:54 -0600

    interactive

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,10 @@ use std::io::{stdin, stdout, Error, Stdin, Write};
 pub const APP_NAME: &str = "CroftSoft Commander © 2022 CroftSoft Inc";
 pub const APP_ABOUT: &str = "Command-line Rust example";
 pub const APP_AUTHOR: &str = "David Wallace Croft, david@croftsoft.com";
+pub const ARG_INTERACTIVE_HELP: &str = "prompt user for inputs";
+pub const ARG_INTERACTIVE_NAME: &str = "interactive";
+pub const ARG_INTERACTIVE_SHORT: char = 'i';
+pub const ARG_INTERACTIVE_TAKES_VALUE: bool = true;
 pub const ARG_NAME_HELP: &str = "name to greet";
 pub const ARG_NAME_NAME: &str = "name";
 pub const ARG_NAME_SHORT: char = 'n';
@@ -33,26 +37,55 @@ pub fn ask(prompt: &str, default: &str) -> String {
 
 #[derive(Debug)]
 pub struct MainArgs {
+  pub interactive: bool,
   pub name_option: Option<String>,
 }
 
-pub fn main(main_args: MainArgs) {
+pub fn main(main_args: MainArgs) -> () {
   // println!("{:?}", main_args);
   // println!("{:#?}", main_args);
+  let greeting: String = make_greeting(main_args);
+  println!("{}", greeting);
+}
+
+// private functions
+
+fn make_greeting(main_args: MainArgs) -> String {
   let name: String = match main_args.name_option {
     Some(arg_name) => arg_name,
-    None => ask(NAME_PROMPT, NAME_DEFAULT),
+    None => {
+      if main_args.interactive {
+        ask(NAME_PROMPT, NAME_DEFAULT)
+      } else {
+        NAME_DEFAULT.to_string()
+      }
+    }
   };
-  println!("Hello, {}!", name);
+  format!("Hello, {}!", name)
 }
 
 #[cfg(test)]
 mod tests {
 
-  // use super::*;
+  use super::*;
 
   #[test]
-  fn test1() {
-    assert_eq!(0, 0);
+  fn test_make_greeting_when_name_none() {
+    let main_args: MainArgs = MainArgs {
+      interactive: false,
+      name_option: None,
+    };
+    let actual_greeting = make_greeting(main_args);
+    assert_eq!(actual_greeting, "Hello, World!");
+  }
+
+  #[test]
+  fn test_make_greeting_when_name_some() {
+    let main_args: MainArgs = MainArgs {
+      interactive: false,
+      name_option: Some(String::from("Test")),
+    };
+    let actual_greeting = make_greeting(main_args);
+    assert_eq!(actual_greeting, "Hello, Test!");
   }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,11 @@ use clap::{App, Arg, ArgMatches};
 use commander::*;
 
 fn main() {
+  let interactive_arg = Arg::new(ARG_INTERACTIVE_NAME)
+    .help(ARG_INTERACTIVE_HELP)
+    .long(ARG_INTERACTIVE_NAME)
+    .short(ARG_INTERACTIVE_SHORT)
+    .takes_value(ARG_INTERACTIVE_TAKES_VALUE);
   let name_arg = Arg::new(ARG_NAME_NAME)
     .help(ARG_NAME_HELP)
     .long(ARG_NAME_NAME)
@@ -10,14 +15,27 @@ fn main() {
     .takes_value(ARG_NAME_TAKES_VALUE);
   let app: App = App::new(APP_NAME)
     .about(APP_ABOUT)
+    .arg(interactive_arg)
     .arg(name_arg)
     .author(APP_AUTHOR);
   let arg_matches: ArgMatches = app.get_matches();
+  let arg_match_interactive: Option<&str> =
+    arg_matches.value_of(ARG_INTERACTIVE_NAME);
   let arg_match_name: Option<&str> = arg_matches.value_of(ARG_NAME_NAME);
+  let interactive: bool = match arg_match_interactive {
+    Some(interactive_string) => match interactive_string {
+      "false" => false,
+      _ => true,
+    },
+    None => true,
+  };
   let name_option: Option<String> = match arg_match_name {
     Some(name) => Some(name.to_string()),
     None => None,
   };
-  let main_args: MainArgs = MainArgs { name_option };
+  let main_args: MainArgs = MainArgs {
+    interactive,
+    name_option,
+  };
   commander::main(main_args);
 }

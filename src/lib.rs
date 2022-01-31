8ab51cec20a7dd012374fa9ee90ca893ac69d715
David Wallace Croft <david@CroftSoft.com>
2022-01-30 18:34:36 -0600

    args from clap

--- src/lib.rs
+++ src/lib.rs
@@ -22,6 +22,34 @@ pub struct MainArgs {
   pub name_option: Option<String>,
 }
 
+pub fn args_from_clap() -> MainArgs {
+  let interactive_arg = Arg::new(ARG_INTERACTIVE_NAME)
+    .help(ARG_INTERACTIVE_HELP)
+    .long(ARG_INTERACTIVE_NAME)
+    .short(ARG_INTERACTIVE_SHORT)
+    .takes_value(ARG_INTERACTIVE_TAKES_VALUE);
+  let name_arg = Arg::new(ARG_NAME_NAME)
+    .help(ARG_NAME_HELP)
+    .long(ARG_NAME_NAME)
+    .short(ARG_NAME_SHORT)
+    .takes_value(ARG_NAME_TAKES_VALUE);
+  let app: App = App::new(APP_NAME)
+    .about(APP_ABOUT)
+    .arg(interactive_arg)
+    .arg(name_arg)
+    .author(APP_AUTHOR);
+  let arg_matches: ArgMatches = app.get_matches();
+  let arg_match_interactive: Option<&str> =
+    arg_matches.value_of(ARG_INTERACTIVE_NAME);
+  let arg_match_name: Option<&str> = arg_matches.value_of(ARG_NAME_NAME);
+  let interactive: bool = !matches!(arg_match_interactive, Some("false"));
+  let name_option: Option<String> = arg_match_name.map(|name| name.to_string());
+  MainArgs {
+    interactive,
+    name_option,
+  }
+}
+
 pub fn ask(prompt: &str, default: &str) -> String {
   loop {
     println!();
@@ -51,31 +79,7 @@ pub fn main_with_args(main_args: MainArgs) {
 }
 
 pub fn main_with_clap() {
-  let interactive_arg = Arg::new(ARG_INTERACTIVE_NAME)
-    .help(ARG_INTERACTIVE_HELP)
-    .long(ARG_INTERACTIVE_NAME)
-    .short(ARG_INTERACTIVE_SHORT)
-    .takes_value(ARG_INTERACTIVE_TAKES_VALUE);
-  let name_arg = Arg::new(ARG_NAME_NAME)
-    .help(ARG_NAME_HELP)
-    .long(ARG_NAME_NAME)
-    .short(ARG_NAME_SHORT)
-    .takes_value(ARG_NAME_TAKES_VALUE);
-  let app: App = App::new(APP_NAME)
-    .about(APP_ABOUT)
-    .arg(interactive_arg)
-    .arg(name_arg)
-    .author(APP_AUTHOR);
-  let arg_matches: ArgMatches = app.get_matches();
-  let arg_match_interactive: Option<&str> =
-    arg_matches.value_of(ARG_INTERACTIVE_NAME);
-  let arg_match_name: Option<&str> = arg_matches.value_of(ARG_NAME_NAME);
-  let interactive: bool = !matches!(arg_match_interactive, Some("false"));
-  let name_option: Option<String> = arg_match_name.map(|name| name.to_string());
-  let main_args: MainArgs = MainArgs {
-    interactive,
-    name_option,
-  };
+  let main_args: MainArgs = args_from_clap();
   main_with_args(main_args);
 }
 

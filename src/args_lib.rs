c00d7cbf19fe9ab5ea64459a508caf8e55972411
David Wallace Croft <david@CroftSoft.com>
2022-01-30 19:28:05 -0600

    args lib

---
+++ src/args_lib.rs
@@ -0,0 +1,8 @@
+use commander::MainArgs;
+
+pub fn make_main_args() -> MainArgs {
+  MainArgs {
+    interactive: true,
+    name_option: None,
+  }
+}
--- src/main-no-clap.rs
+++ src/main-no-clap.rs
@@ -1,9 +1,9 @@
-mod clap_lib;
+mod args_lib;
 
-use clap_lib::*;
+use args_lib::*;
 use commander::*;
 
 fn main() {
-  let main_args: MainArgs = args_from_clap();
+  let main_args: MainArgs = make_main_args();
   commander::main(main_args);
 }

a2bc050cb7f28639fe7392135a357d1c3c839e96
David Wallace Croft <david@CroftSoft.com>
2022-01-30 18:31:01 -0600

    thin main

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 use std::io::{stdin, stdout, Error, Stdin, Write};
+// https://docs.rs/clap/latest/clap/
+use clap::{App, Arg, ArgMatches};
 
 pub const APP_NAME: &str = "CroftSoft Commander © 2022 CroftSoft Inc";
 pub const APP_ABOUT: &str = "Command-line Rust example";
@@ -13,6 +15,12 @@ pub const ARG_NAME_SHORT: char = 'n';
 pub const ARG_NAME_TAKES_VALUE: bool = true;
 pub const NAME_DEFAULT: &str = "World";
 pub const NAME_PROMPT: &str = "What is your name? [World]: ";
+
+#[derive(Debug)]
+pub struct MainArgs {
+  pub interactive: bool,
+  pub name_option: Option<String>,
+}
 
 pub fn ask(prompt: &str, default: &str) -> String {
   loop {
@@ -35,17 +43,40 @@ pub fn ask(prompt: &str, default: &str) -> String {
   }
 }
 
-#[derive(Debug)]
-pub struct MainArgs {
-  pub interactive: bool,
-  pub name_option: Option<String>,
-}
-
-pub fn main(main_args: MainArgs) {
+pub fn main_with_args(main_args: MainArgs) {
   // println!("{:?}", main_args);
   // println!("{:#?}", main_args);
   let greeting: String = make_greeting(main_args);
   println!("{}", greeting);
+}
+
+pub fn main_with_clap() {
+  let interactive_arg = Arg::new(ARG_INTERACTIVE_NAME)
+    .help(ARG_INTERACTIVE_HELP)
+    .long(ARG_INTERACTIVE_NAME)
+    .short(ARG_INTERACTIVE_SHORT)
+    .takes_value(ARG_INTERACTIVE_TAKES_VALUE);
+  let name_arg = Arg::new(ARG_NAME_NAME)
+    .help(ARG_NAME_HELP)
+    .long(ARG_NAME_NAME)
+    .short(ARG_NAME_SHORT)
+    .takes_value(ARG_NAME_TAKES_VALUE);
+  let app: App = App::new(APP_NAME)
+    .about(APP_ABOUT)
+    .arg(interactive_arg)
+    .arg(name_arg)
+    .author(APP_AUTHOR);
+  let arg_matches: ArgMatches = app.get_matches();
+  let arg_match_interactive: Option<&str> =
+    arg_matches.value_of(ARG_INTERACTIVE_NAME);
+  let arg_match_name: Option<&str> = arg_matches.value_of(ARG_NAME_NAME);
+  let interactive: bool = !matches!(arg_match_interactive, Some("false"));
+  let name_option: Option<String> = arg_match_name.map(|name| name.to_string());
+  let main_args: MainArgs = MainArgs {
+    interactive,
+    name_option,
+  };
+  main_with_args(main_args);
 }
 
 // private functions

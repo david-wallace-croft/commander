7880cb731b6379fa6d6738d24d676c7b1deb50aa
David Wallace Croft <david@CroftSoft.com>
2022-01-31 20:59:45 -0600

    name option

--- src/args_lib.rs
+++ src/args_lib.rs
@@ -5,14 +5,23 @@ use std::env;
 
 pub fn make_main_args() -> MainArgs {
   let args: Vec<String> = env::args().collect();
-  println!("{:?}", args);
-  let length: usize = args.len();
-  println!("Args length = {}", length);
+  // println!("{:?}", args);
+  // let length: usize = args.len();
+  // println!("Args length = {}", length);
   let help_wanted: bool = args.contains(&String::from("--help"))
     || args.contains(&String::from("-h"));
+  let mut name_option = None;
+  for index in 2..args.len() {
+    let option: &String = &args[index - 1];
+    let value: &String = &args[index];
+    if (option.eq("-n") || option.eq("--name")) && !value.starts_with('-') {
+      name_option = Some(value.to_string());
+      break;
+    }
+  }
   MainArgs {
     help_wanted,
     interactive: true,
-    name_option: None,
+    name_option,
   }
 }
